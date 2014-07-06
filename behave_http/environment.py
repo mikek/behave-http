a7e875ab87ec32c5ddac9a63feadbfeeda07a60a
Mikhail Kolesnik <mike@openbunker.org>
2014-07-06 10:24:50 +0400

    Add environment.py

---
+++ behave_http/environment.py
@@ -0,0 +1,13 @@
+def before_scenario(context, scenario):
+    # Seed empty HTTP headers so steps do not need to check and create.
+    context.headers = {}
+
+    # Seed empty Jinja2 template data so steps do not need to check and create.
+    context.template_data = {}
+
+    # Default repeat attempt counts and delay for polling GET.
+    context.n_attempts = 20
+    context.pause_between_attempts = 0.1
+
+    # Do not authenticate by dafault
+    context.auth = None
