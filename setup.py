2c698ceb3b830d3fcccbcfc28e0ff57546b76963
Mykhailo Kolesnyk <mykhailo.kolesnyk@divio.ch>
2015-11-07 14:52:49 +0200

    Add Python 3.5 support

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Development Status :: 4 - Beta',
         'Natural Language :: English',
         'Environment :: Web Environment',
