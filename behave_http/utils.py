f513b7df8fe38796475343df605d5bbdcc6dd03a
Grigory Starinkin <gstarinkin@alertlogic.com>
2016-03-21 13:28:26 +0000

    allow specify paths with more than 1 segments

--- behave_http/utils.py
+++ behave_http/utils.py
@@ -43,9 +43,9 @@ def dereference_step_parameters_and_data(f):
     return wrapper
 
 
-def append_path(url, url_path_segment):
-    target = URL(url_path_segment)
-    url = url.add_path_segment(target.path())
+def append_path(url, url_path):
+    target = URL(url_path)
+    url = url.path(target.path())
     if target.query():
         url = url.query(target.query())
     return url.as_string()
